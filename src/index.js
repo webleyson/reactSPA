c2998e42e0e5db71a068bc58467d7c694d98147d
Matt Webley <matt@ccsw.co.uk>
2019-02-08 11:44:53 +0000

    last commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import './custom.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
