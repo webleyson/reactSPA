e2be69d9dd5aad4f22d606db5a32703f027f698e
Matt Webley <matt@ccsw.co.uk>
2019-02-08 11:38:28 +0000

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import 'bootstrap/dist/css/bootstrap.css';
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
